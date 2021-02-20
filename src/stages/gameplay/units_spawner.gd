cb6568ec673abd3a7bc5b10b5420d1e8faa537d8
Konrad Nowakowski <konrad.x92@gmail.com>
2021-02-20 23:59:38 +0000

    Add looped unit spawner

--- src/stages/gameplay/units_spawner.gd
+++ src/stages/gameplay/units_spawner.gd
@@ -15,6 +15,8 @@ func _spawn_enemies() -> void:
 	yield(get_tree().create_timer(4.0), "timeout")
 	_spawn_unit(SpaceshipScene, Vector2(200.0, 20.0))
 	_spawn_unit(SpaceshipScene, Vector2(-200.0, 20.0))
+	yield(get_tree().create_timer(4.0), "timeout")
+	_spawn_enemies()
 
 func _spawn_unit(p_type: PackedScene, p_from: Vector2) -> void:
 	var unit_node = p_type.instance()
