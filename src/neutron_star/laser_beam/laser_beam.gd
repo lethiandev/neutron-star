1cf2bfbec099e0300a3a1869a996a9cacacff9ce
Konrad Nowakowski <konrad.x92@gmail.com>
2021-02-21 21:45:30 +0000

    Add laser beam scene

---
+++ src/neutron_star/laser_beam/laser_beam.gd
@@ -0,0 +1,21 @@
+extends Node2D
+tool
+
+const BeamBegin = preload("./beam_begin.png")
+const BeamEnd = preload("./beam_end.png")
+
+export(float) var duration: float = 0.4
+export(Vector2) var target: Vector2 = Vector2() \
+	setget set_target, get_target
+
+func _draw() -> void:
+	draw_line(Vector2(), target, Color(1.0, 1.0, 1.0), 10)
+	draw_texture(BeamBegin, BeamBegin.get_size() * -0.5)
+	draw_texture(BeamEnd, target - BeamEnd.get_size() * 0.5)
+
+func set_target(p_target: Vector2) -> void:
+	target = p_target
+	update()
+
+func get_target() -> Vector2:
+	return target
