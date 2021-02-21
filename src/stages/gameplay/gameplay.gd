f71567c406626e3c7b82485d155da0478871b049
Konrad Nowakowski <konrad.x92@gmail.com>
2021-02-21 14:04:42 +0000

    Add cooldown feature

--- src/stages/gameplay/gameplay.gd
+++ src/stages/gameplay/gameplay.gd
@@ -7,26 +7,44 @@ var action_group_map = {
 	"action_p": UnitBase.UNIT_GROUP_P,
 }
 
+var cooldown: float = 0.0
+var health: float = 1.0
+
 func _init() -> void:
 	randomize()
 
 func _process(p_delta: float) -> void:
+	if cooldown < 0.1:
+		_process_game_input()
+	_process_game_state(p_delta)
+
+func _process_game_input() -> void:
 	for action in action_group_map:
 		if Input.is_action_just_pressed(action):
 			_process_unit_group(action_group_map[action])
+
+func _process_game_state(p_delta: float) -> void:
+	_populate_interface_state()
+	
+	cooldown = max(0.0, cooldown - p_delta)
+	health = min(1.0, health + p_delta * 0.5)
+
+func _populate_interface_state() -> void:
+	$Interface/CenterContainer/CooldownBar.progress = cooldown
+	$Interface/CenterContainer/HealthBar.progress = health
 
 func _process_unit_group(p_group: String) -> void:
 	var nodes = get_tree().get_nodes_in_group(p_group)
 	if not nodes.empty():
 		var front_node = nodes.front()
-		_process_unit_hit(front_node)
+		_handle_unit_hit(front_node)
 	else:
-		_process_miss()
+		_handle_unit_miss()
 
-func _process_unit_hit(p_unit: Node) -> void:
+func _handle_unit_hit(p_unit: Node) -> void:
 	if p_unit is UnitBase:
 		p_unit.hit()
 		$Camera2D.shake_low()
 
-func _process_miss() -> void:
-	pass
+func _handle_unit_miss() -> void:
+	cooldown = 1.0
