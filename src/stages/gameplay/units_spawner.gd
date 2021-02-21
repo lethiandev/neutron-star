a5c4089dac55a37ed7efe1914fab50aa5ebea63a
Konrad Nowakowski <konrad.x92@gmail.com>
2021-02-21 21:29:48 +0000

    Add more enemy waves

--- src/stages/gameplay/units_spawner.gd
+++ src/stages/gameplay/units_spawner.gd
@@ -6,6 +6,9 @@ const MothershipBossScene = preload("res://units/mothership_boss/mothership_boss
 func _ready() -> void:
 	yield(_delay(2.0), "completed")
 	yield(_spawn_wave_1(), "completed")
+	yield(_spawn_wave_2(), "completed")
+	yield(_spawn_wave_3(), "completed")
+	yield(_spawn_wave_4(), "completed")
 	yield(_spawn_mothership_boss(), "completed")
 
 func _spawn_wave_1() -> void:
@@ -14,6 +17,47 @@ func _spawn_wave_1() -> void:
 	yield(_spawn_spaceship(2.0, 3.0, Vector2(200.0 * -side, 80.0)), "completed")
 	yield(_spawn_spaceship(2.0, 2.5, Vector2(220.0 * side, -20.0)), "completed")
 	yield(_spawn_spaceship(2.0, 2.5, Vector2(220.0 * -side, -20.0)), "completed")
+	yield(_delay(2.0), "completed")
+
+func _spawn_wave_2() -> void:
+	var side = 1.0 if randf() > 0.5 else -1.0
+	yield(_spawn_spaceship(1.5, 2.0, Vector2(200.0 * side, 40.0)), "completed")
+	yield(_spawn_spaceship(1.5, 2.0, Vector2(200.0 * -side, 40.0)), "completed")
+	yield(_spawn_spaceship(1.5, 2.0, Vector2(220.0 * side, -40.0)), "completed")
+	yield(_spawn_spaceship(1.5, 2.0, Vector2(220.0 * -side, -40.0)), "completed")
+	yield(_spawn_spaceship(2.0, 2.0, Vector2(200.0 * side, 60.0)), "completed")
+	yield(_spawn_spaceship(1.5, 2.0, Vector2(200.0 * -side, -60.0)), "completed")
+	yield(_spawn_spaceship(2.0, 2.0, Vector2(200.0 * side, 80.0)), "completed")
+	yield(_spawn_spaceship(1.5, 2.0, Vector2(200.0 * -side, 80.0)), "completed")
+	yield(_delay(2.0), "completed")
+
+func _spawn_wave_3() -> void:
+	var side = 1.0 if randf() > 0.5 else -1.0
+	yield(_spawn_spaceship(1.0, 2.0, Vector2(180.0 * side, -80.0)), "completed")
+	yield(_spawn_spaceship(0.5, 2.0, Vector2(180.0 * -side, -80.0)), "completed")
+	yield(_spawn_spaceship(1.0, 2.0, Vector2(180.0 * side, 80.0)), "completed")
+	yield(_spawn_spaceship(0.5, 2.0, Vector2(180.0 * -side, -80.0)), "completed")
+	yield(_spawn_spaceship(0.5, 2.0, Vector2(180.0 * side, -60.0)), "completed")
+	yield(_spawn_spaceship(0.2, 2.0, Vector2(180.0 * -side, -60.0)), "completed")
+	yield(_spawn_spaceship(0.5, 2.0, Vector2(180.0 * side, 60.0)), "completed")
+	yield(_spawn_spaceship(0.2, 2.0, Vector2(180.0 * -side, 60.0)), "completed")
+	yield(_spawn_spaceship(0.5, 2.0, Vector2(200.0 * side, -40.0)), "completed")
+	yield(_spawn_spaceship(0.2, 2.0, Vector2(200.0 * -side, -40.0)), "completed")
+	yield(_spawn_spaceship(0.5, 2.0, Vector2(220.0 * side, 40.0)), "completed")
+	yield(_spawn_spaceship(0.2, 2.0, Vector2(220.0 * -side, 40.0)), "completed")
+	yield(_delay(2.0), "completed")
+
+func _spawn_wave_4() -> void: 
+	var side = 1.0 if randf() > 0.5 else -1.0
+	yield(_spawn_spaceship(0.0, 1.8, Vector2(180.0 * side, -80.0)), "completed")
+	yield(_spawn_spaceship(1.0, 1.8, Vector2(180.0 * -side, -80.0)), "completed")
+	yield(_spawn_spaceship(0.0, 1.8, Vector2(180.0 * side, 20.0)), "completed")
+	yield(_spawn_spaceship(1.0, 1.8, Vector2(180.0 * -side, 20.0)), "completed")
+	yield(_spawn_spaceship(0.0, 1.6, Vector2(180.0 * side, 60.0)), "completed")
+	yield(_spawn_spaceship(0.8, 1.6, Vector2(180.0 * -side, 60.0)), "completed")
+	yield(_spawn_spaceship(0.0, 1.6, Vector2(180.0 * side, -60.0)), "completed")
+	yield(_spawn_spaceship(0.8, 1.6, Vector2(180.0 * -side, -60.0)), "completed")
+	yield(_delay(2.0), "completed")
 
 func _spawn_mothership_boss() -> void:
 	var boss_node = MothershipBossScene.instance()
