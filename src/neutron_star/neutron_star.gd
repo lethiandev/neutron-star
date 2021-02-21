e7808f26e00560ac40f61ca8188c186f5c8c5ef0
Konrad Nowakowski <konrad.x92@gmail.com>
2021-02-21 22:16:50 +0000

    Add laser beam shooting

--- src/neutron_star/neutron_star.gd
+++ src/neutron_star/neutron_star.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+const LasterBeam = preload("res://neutron_star/laser_beam/laser_beam.gd")
+
 signal unit_hitted(unit)
 
 func _ready() -> void:
@@ -12,3 +14,11 @@ func _on_area_entered(p_area: Area2D) -> void:
 func _handle_unit_collision(p_unit: UnitBase) -> void:
 	emit_signal("unit_hitted", p_unit)
 	p_unit.destroy(true)
+
+func shoot(p_target: Vector2) -> void:
+	var laser_beam = LasterBeam.new()
+	var target = p_target - global_position
+	var offset = target.normalized() * 18.0
+	laser_beam.position = offset
+	laser_beam.target = target - offset
+	get_parent().add_child(laser_beam)
